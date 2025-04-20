f8fb8f382b19ca7901c8494c8b9415ffcf0d6f3f
xarop-pa-toss <rpgdsantos@gmail.com>
2025-04-20 23:39:55 +0100

    Fix main_test.ts

--- backend/main.ts
+++ backend/main.ts
@@ -1,16 +1,7 @@
 import { Hono } from 'hono';
-import { generateBackgroundImage } from "./bgImageGenerator.ts";
-
 
 const app = new Hono();
 
-app.get('/', async (c) => {
-  const img = await generateBackgroundImage(1024, 768, 33);
-  return c.body(img, {
-    headers: {
-      "Content-Type": "image/png"
-    }
-  });
-})
+app.get('/', (c) => c.text('Hello from Hono on Deno Deploy!'))
 
 await Deno.serve({port: 8787}, app.fetch);
